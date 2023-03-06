ed89aeb8c3247be8b72816ff2ec6379ff05ed909
saikumarpb <saipusarla7094@gmail.com>
2023-03-06 18:46:16 +0530

    Add user table entity and migration

---
+++ api/ecommerce/src/main/java/sai/ecommerce/domain/User.java
@@ -0,0 +1,40 @@
+package sai.ecommerce.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Date;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false)
+  private Integer id;
+
+  private String name;
+
+  private String email;
+
+  private String password;
+
+  @Column(name = "created_at")
+  @CreationTimestamp
+  private Date createdAt;
+
+  @Column(name = "updated_at")
+  @UpdateTimestamp
+  private Date updatedAt;
+}
